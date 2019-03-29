130c44ae9c5d2dcb1f3efffb5c170d6fd2c1a4a0
alex-filkov <alex.filkov@paml.ca>
2019-03-29 14:51:21 -0700

    Update crmtest1.js

--- crmtest1.js
+++ crmtest1.js
@@ -7,7 +7,9 @@ function remoteFunction()
 
 function readMLmembers()
 {
+  console.log('readMLmembers called');
   var url = 'https://paml-beta.crm.dynamics.com/api/data/v9.0/listmembers?$select=entitytype,listmemberid&$filter=_listid_value%20eq%20%2782E1A099-214C-E911-A956-000D3A124EF5%27';
+  console.log('readMLmembers url' + url);
   $.get( url, function( data ) {
     console.log('data retrieved');
 });
